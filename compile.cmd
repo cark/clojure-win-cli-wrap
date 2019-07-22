833dac820d4ddd2d93c4d6c20db35c35ded786c8
cark <carkhy@gmail.com>
2019-07-22 08:18:37 +0200

    Add error detection and zipping to compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,12 @@
-nim c -d:cmdlet=clojure -d:release --opt:size -o:clojure.exe clojure.nim 
-nim c -d:cmdlet=clj -d:release --opt:size -o:clj.exe clojure.nim 
+nim c -d:cmdlet=clojure -d:release --opt:size -o:clojure.exe clojure.nim
+@if ERRORLEVEL 1 GOTO error
+nim c -d:cmdlet=clj -d:release --opt:size -o:clj.exe clojure.nim
+@if ERRORLEVEL 1 GOTO error
+
+@rd /S /Q release
+@md release
+7z a release\clojure-win-cli-wrap.zip clojure.exe clj.exe
+
+@GOTO:EOF
+:error
+@ECHO ******** STOP ON ERROR *********
